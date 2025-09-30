2b7d02e3af91609f224067407caf7fe07e03a003
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-09-30 12:25:23 -0500

    fifth commit

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+import java.util.*;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String employeeName = scanner.nextLine();
+        System.out.println("Enter your hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+        System.out.println("Enter your pay rate: ");
+        double payRate = scanner.nextDouble();
+        System.out.println("Employee Name: "+employeeName);
+
+    }
+
+    public static void grossPay(){
+        System.out.println("Gross Pay: "+ );
+    }
+
+}
